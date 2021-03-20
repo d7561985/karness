030d3a3ce60d2967ed30ca741976e072dbab603d
d7561985 <d7561985@gmail.com>
2021-03-20 18:41:26 +0200

    executor/grpc: internal test covered all functionalities

--- go.mod
+++ go.mod
@@ -4,8 +4,11 @@ go 1.16
 
 require (
 	github.com/fullstorydev/grpcurl v1.8.0
+	github.com/grpc-ecosystem/grpc-gateway v1.9.2
 	github.com/jhump/protoreflect v1.6.1
-	google.golang.org/grpc v1.30.0
+	github.com/stretchr/testify v1.7.0 // indirect
+	google.golang.org/grpc v1.36.0
+	google.golang.org/grpc/examples v0.0.0-20210318230139-bce1cded4b05
 	k8s.io/api v0.0.0-20210306132658-3687c906b8c9
 	k8s.io/apimachinery v0.0.0-20210306132128-283a3268598b
 	k8s.io/client-go v0.0.0-20210306133319-1745c9faaaff
--- pkg/executor/grpc_test.go
+++ pkg/executor/grpc_test.go
@@ -1,17 +1,79 @@
 package executor
 
 import (
+	"context"
 	"fmt"
+	"log"
+	"net"
 	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"google.golang.org/grpc"
+	pb "google.golang.org/grpc/examples/helloworld/helloworld"
+	"google.golang.org/grpc/reflection"
 )
 
+type fixture struct {
+	err error
+	res *pb.HelloReply
+
+	cb func(*pb.HelloRequest)
+}
+
+type server struct {
+	pb.UnimplementedGreeterServer
+	fixture
+}
+
+func (s server) SayHello(_ context.Context, req *pb.HelloRequest) (*pb.HelloReply, error) {
+	s.cb(req)
+	return s.res, s.err
+}
+
+func createServer(fx fixture) (net.Listener, *grpc.Server) {
+	l, err := net.Listen("tcp", ":0")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	s := grpc.NewServer()
+
+	// Enable reflection
+	reflection.Register(s)
+
+	pb.RegisterGreeterServer(s, &server{fixture: fx})
+
+	go func() {
+		if err := s.Serve(l); err != nil {
+			log.Fatalf("Server exited with error: %v", err)
+		}
+	}()
+
+	return l, s
+}
+
+// TestGRPC shows all flow about server requests
 func TestGRPC(t *testing.T) {
-	const target = "sportsbook-settings-sv.odds-compiler.svc.cluster.local:9000"
-	const symbol = "egt.oddscompiler.sportsbooksettings.v3.public.InfoService/GetEventInfo"
+	name := "TEST_NAME"
+
+	l, srv := createServer(fixture{
+		err: nil,
+		res: &pb.HelloReply{Message: "OK"},
+		cb: func(req *pb.HelloRequest) {
+			assert.Equal(t, name, req.Name)
+		},
+	})
+
+	defer l.Close()
+	defer srv.Stop()
+
+	fmt.Println(l.Addr().String())
+
+	const symbol = "helloworld.Greeter/SayHello"
 
 	g := NewGRPC()
 
-	res := g.Go(target, symbol, `{"id":"1"}`)
+	err := g.Go(l.Addr().String(), symbol, fmt.Sprintf(`{"name":"%s"}`, name))
 
-	fmt.Println(res)
+	assert.NoError(t, err)
 }
